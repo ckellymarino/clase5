7d95addc34cd2668e6de6e4df4ecbe53050f4a2c
Kelly Carolina <ckellymarino@gmail.com>
2024-02-28 20:19:27 -0300

    intento fallido

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
-
 alert("Bienvenido! ¿Estas preparado para comenzar?");
 
 let nombre = prompt("Ingresa tu nombre");
+
+let anioDeNacimiento = prompt("Ingresá tu año de nacimiento") 
+
+let rataInicial = 1960
+let bueyInicial = 1961
+
+
+while (anioDeNacimiento != "ESC") {
+    switch (anioDeNacimiento) {
+        case "RATA":
+            function signoRata(rataInicial){
+            for (let i = 0; i<=12; i++) {
+                rataInicial += i;
+            }
+            return rataInicial;
+            alert("Segun el calendario chino sos RATA");
+            }
+        // case "BUEY":
+        //     for (let i = 0; i<=12; i++) {
+        //         bueyInicialInicial += i;
+        //     } return bueyInicialInicial;
+        //     alert("Segun el calendario chino sos BUEY")
+
+        
+        default:
+            alert("Sos muy viejo :) ");
+        break;    
+    } 
+}
+
+let anioDeNacimiento2 = prompt("Ingresa nuevamente tu año de nacimiento");
+
+
+
+
 
